766a282c82f7abb912d53b9e43792ef798cbd231
sanjeev8521 <pandeysanjeev2001@gmail.com>
2025-01-30 09:43:44 +0530

    Initial commit

---
+++ YouTube-subscribers-main/src/data.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+      "name": "pratik ",
+      "subscribedChannel": "CNET"
+    },
+    {
+      "name": "sanjeev",
+      "subscribedChannel": "freeCodeCamp.org"
+    },
+    {
+      "name": "abs",
+      "subscribedChannel": "Sentex"
+    }
+  ]
+
+module.exports = data;
